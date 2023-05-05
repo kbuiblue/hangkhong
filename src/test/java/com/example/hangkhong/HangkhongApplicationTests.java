490a0472812b798e3400fbc2bb975e34e5a35d63
bqkhanh <kbuiblue@gmail.com>
2023-05-05 09:11:21 +0700

    Initialize hangkhong

---
+++ src/test/java/com/example/hangkhong/HangkhongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangkhong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangkhongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
